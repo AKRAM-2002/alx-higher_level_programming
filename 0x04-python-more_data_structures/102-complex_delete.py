d54080bf20fc20e94ff52e7907faa46e1fd6fdf3
AKRAM <akramboutzouga@gmail.com>
2023-10-16 05:51:46 -0700

    add files

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,13 +2,11 @@
 def complex_delete(a_dictionary, value):
     keys_to_delete = []
 
-    new_dict = a_dictionary.copy()
-
-    for key, val in new_dict.items():
+    for key, val in a_dictionary.items():
         if val == value:
             keys_to_delete.append(key)
 
     for key in keys_to_delete:
-        del new_dict[key]
+        del a_dictionary[key]
 
-    return new_dict
+    return a_dictionary
