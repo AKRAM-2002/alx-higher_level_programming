b870e2a5b491f30834f94735028283882c02be43
Akram <akramboutzouga@gmail.com>
2024-01-18 11:22:42 -0800

    update

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,5 @@
 -- A Query that updates the score 
 -- of a record in the table second_table
-UPDATE second_table SET score = 10 where name = "Bob";
+UPDATE second_table
+SET score = 10
+where name = "Bob"
