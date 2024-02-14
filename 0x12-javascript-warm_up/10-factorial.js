349c8e00683cf7e4dfaa05cd0500efbfeede9911
akram <akramboutzouga@gmail.com>
2024-02-14 10:36:35 +0100

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-function factorial(n) {
-    if (isNaN(n)) {
-        return 1;
-    }
-    n = parseInt(n, 10);
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  n = parseInt(n, 10);
 
-    if (n === 0 || n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 const input = args[0];
 const result = factorial(input);
 if (!isNaN(result) && isFinite(result)) {
-    console.log(result);
+  console.log(result);
 } else {
-    console.log("Infinity");
+  console.log('Infinity');
 }
