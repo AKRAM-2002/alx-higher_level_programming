3ec50e412ba336e5e2e318efaf813936fbe73769
akram <akramboutzouga@gmail.com>
2024-03-29 15:30:39 +0100

    test

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # A script that displays all HTTP methods the server will accept
-allow_header=$(curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-)
-echo "$allow_header"
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
