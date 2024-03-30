e2d6fe1772c25208e742dd79518035b7acaf0878
akram <akramboutzouga@gmail.com>
2024-03-30 12:21:01 +0100

    done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """
-
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
 """
 
 
@@ -12,16 +16,15 @@ if __name__ == '__main__':
     owner = argv[2]
     i = 0
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
 
-    
-    response = get(url)
+    response = get(URL)
+    json = response.json()
 
-    if response.status_code == 200:
-        commits = response.json()[:10]  # Get the 10 most recent commits
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print(f"Error: {response.status_code}")
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
