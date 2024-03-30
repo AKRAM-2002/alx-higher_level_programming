610d290c2b9753a5e8bb5e76bc4e1dde5e257f99
akram <akramboutzouga@gmail.com>
2024-03-30 12:24:14 +0100

    done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 # script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter
+from urllib import request, parse
 import sys
-from  urllib import request, parse
+
 
 if __name__ == "__main__":
-    
-    data = parse.urlencode({'email': sys.argv[2]})
-    data = data.encode('utf-8')
-    req = request.Request(sys.argv[1], data=data, method='POST')
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
-        html = response.read().decode('utf-8')
-        print(html)
-
+        body = response.read()
+        print(body.decode('utf-8'))
 
 
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 # script that fetches https://alx-intranet.hbtn.io/status using requests package
 
+
 import requests
+
+
 if __name__ == "__main__":
-
-    url = 'https://alx-intranet.hbtn.io/status'
-    with requests.get(url) as res:
-        html = res.text
-        print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
